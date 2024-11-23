7b9db084e103b3b4abf9dfcb5d00ccc4c3247fc2
Ananya Teshome <112172211+anuteshome@users.noreply.github.com>
2024-11-22 16:08:17 -0800

    updated App.js

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
 const http=require("http");
 const url =require("url");
 const fs=require("fs");
+const mime=require("mime-types").lookup
 
-const MyServer=http.createServer(function(req,res){
-if(req.url === "/"){
-  res.write("<h1>this is Home page</h1>");
-  res.end();
-}else if(req.url === "/about"){
-  res.write("<h1>This is abot page</h1>");
-  res.end();
-}else{
-  res.write("<h1>page not found</h1>");
-  res.end();
-}
+const MyServer= http.createServer(function(req,res){
 
-}).listen(5000);
+  let filepath=req.url;
+  console.log(filepath);
+
+  if(req.url == "/"){
+    filepath = '/index.html';
+    }
+  const RequestedFile=  "./layout" + filepath;
+
+  fs.readFile(RequestedFile, (err,data) => {
+    if (err) {
+        res.end('<h1>Server Error</h1>');
+    } else {
+      let fileType = mime(RequestedFile);
+        res.writeHead(200, { 'Content-Type': fileType });
+        res.end(data);
+    }
+});
+
+}).listen(8000);
 
